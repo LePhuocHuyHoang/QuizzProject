1c91f014495fcfac66efcc89b3371063f161bd67
Lê Phước Huy Hoàng <lehoang023479@gmail.com>
2025-02-09 11:21:28 +0700

    video 22

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,7 +1,12 @@
 import React from "react";
+import UserInfor from "./UserInfor";
 class MyComponent extends React.Component {
   render() {
-    return <div>my first component {Math.random()}</div>;
+    return (
+      <div>
+        <UserInfor />
+      </div>
+    );
   }
 }
 export default MyComponent;
