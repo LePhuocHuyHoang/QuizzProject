6edff49745a9aa0754f7f9b575e57437dd4216f5
Lê Phước Huy Hoàng <lehoang023479@gmail.com>
2025-02-09 17:24:32 +0700

    video 25

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,10 +1,20 @@
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 class MyComponent extends React.Component {
+  state = {
+    listUsers: [
+      { id: 1, name: "Huy Hoàng", age: "35" },
+      { id: 2, name: "Hoàng Long", age: "22" },
+      { id: 3, name: "Hoàng Hùng", age: "10" },
+    ],
+  };
   render() {
     return (
       <div>
         <UserInfor />
+        <br /> <br />
+        <DisplayInfor listUsers={this.state.listUsers} />
       </div>
     );
   }
