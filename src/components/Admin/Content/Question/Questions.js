6f7c545383001bfd1f4efafabbe64932f448b746
Lê Phước Huy Hoàng <lehoang023479@gmail.com>
2025-02-19 21:23:30 +0700

    video 95

--- src/components/Admin/Content/Question/Questions.js
+++ src/components/Admin/Content/Question/Questions.js
@@ -7,6 +7,7 @@ import { FaMinusCircle } from "react-icons/fa";
 import { FaMinus } from "react-icons/fa";
 import { RiImageAddFill } from "react-icons/ri";
 import { v4 as uuidv4 } from "uuid";
+import _, { find } from "lodash";
 
 const Questions = (props) => {
   const options = [
@@ -21,22 +22,47 @@ const Questions = (props) => {
       description: "question 1",
       imageFile: "",
       imageName: "",
-      answers: [
-        { id: uuidv4(), description: "answer 1", isCorrect: false },
-        { id: uuidv4(), description: "answer 2", isCorrect: false },
-      ],
-    },
-    {
-      id: uuidv4(),
-      description: "question 2",
-      imageFile: "",
-      imageName: "",
-      answers: [
-        { id: uuidv4(), description: "answer 1", isCorrect: false },
-        { id: uuidv4(), description: "answer 2", isCorrect: false },
-      ],
+      answers: [{ id: uuidv4(), description: "answer 1", isCorrect: false }],
     },
   ]);
+  const handleAddRemoveQuestion = (type, id) => {
+    if (type === "ADD") {
+      const newQuestion = {
+        id: uuidv4(),
+        description: "question 1",
+        imageFile: "",
+        imageName: "",
+        answers: [{ id: uuidv4(), description: "", isCorrect: false }],
+      };
+      setQuestions([...questions, newQuestion]);
+    }
+    if (type === "REMOVE") {
+      let questionClone = _.cloneDeep(questions);
+      questionClone = questionClone.filter((item) => item.id !== id);
+      setQuestions(questionClone);
+    }
+  };
+  const handleAddRemoveAnswer = (type, questionId, answerId) => {
+    let questionClone = _.cloneDeep(questions);
+    if (type === "ADD") {
+      const newAnswer = {
+        id: uuidv4(),
+        description: "",
+        isCorrect: false,
+      };
+      let index = questionClone.findIndex((item) => item.id === questionId);
+      questionClone[index].answers.push(newAnswer);
+      setQuestions(questionClone);
+    }
+    if (type === "REMOVE") {
+      let index = questionClone.findIndex((item) => item.id === questionId);
+      questionClone[index].answers = questionClone[index].answers.filter(
+        (item) => item.id !== answerId
+      );
+      setQuestions(questionClone);
+    }
+    console.log("question", questions);
+  };
   console.log("question", questions);
   return (
     <div className="questions-container">
@@ -52,71 +78,89 @@ const Questions = (props) => {
           />
         </div>
         <div className="mt-3 mb-2">Add questions: </div>
-        <div className="q-main mb-5">
-          <div className="questions-content">
-            <div className="form-floating description">
-              <input
-                type="type"
-                className="form-control"
-                placeholder="name@example.com"
-              />
-              <label>Question's Description</label>
-            </div>
-            <div className="group-upload">
-              <label>
-                <RiImageAddFill className="label-upload" />
-              </label>
-              <input type={"file"} hidden />
-              <span>0 file is uploaded</span>
-            </div>
-            <div className="btn-add">
-              <span>
-                <AiFillPlusCircle className="icon-add" />
-              </span>
-              <span>
-                <FaMinusCircle className="icon-remove" />
-              </span>
-            </div>
-          </div>
-          <div className="answers-content">
-            <input className="form-check-input iscorrect" type="checkbox" />
-            <div className="form-floating answer-name">
-              <input
-                type="type"
-                className="form-control"
-                placeholder="name@example.com"
-              />
-              <label>Answer 1</label>
-            </div>
-            <div className="btn-group">
-              <span>
-                <FaPlus className="icon-add" />
-              </span>
-              <span>
-                <FaMinus className="icon-remove" />
-              </span>
-            </div>
-          </div>
-          <div className="answers-content">
-            <input className="form-check-input iscorrect" type="checkbox" />
-            <div className="form-floating answer-name">
-              <input
-                type="type"
-                className="form-control"
-                placeholder="name@example.com"
-              />
-              <label>Answer 1</label>
-            </div>
-            <div className="btn-group">
-              <span>
-                <FaPlus className="icon-add" />
-              </span>
-              <span>
-                <FaMinus className="icon-remove" />
-              </span>
-            </div>
-          </div>
-        </div>
+        {questions &&
+          questions.length > 0 &&
+          questions.map((question, index) => {
+            return (
+              <div key={question.id} className="q-main mb-3">
+                <div className="questions-content">
+                  <div className="form-floating description">
+                    <input
+                      type="type"
+                      className="form-control"
+                      placeholder="name@example.com"
+                      value={question.description}
+                    />
+                    <label>Question {index + 1} 's Description</label>
+                  </div>
+                  <div className="group-upload">
+                    <label>
+                      <RiImageAddFill className="label-upload" />
+                    </label>
+                    <input type={"file"} hidden />
+                    <span>0 file is uploaded</span>
+                  </div>
+                  <div className="btn-add">
+                    <span onClick={() => handleAddRemoveQuestion("ADD", "")}>
+                      <AiFillPlusCircle className="icon-add" />
+                    </span>
+                    {questions.length > 1 && (
+                      <span
+                        onClick={() =>
+                          handleAddRemoveQuestion("REMOVE", question.id)
+                        }
+                      >
+                        <FaMinusCircle className="icon-remove" />
+                      </span>
+                    )}
+                  </div>
+                </div>
+                {question.answers &&
+                  question.answers.length > 0 &&
+                  question.answers.map((answer, index) => {
+                    return (
+                      <div key={answer.id} className="answers-content">
+                        <input
+                          className="form-check-input iscorrect"
+                          type="checkbox"
+                        />
+                        <div className="form-floating answer-name">
+                          <input
+                            value={answer.description}
+                            type="type"
+                            className="form-control"
+                            placeholder="name@example.com"
+                          />
+                          <label>Answer {index + 1}</label>
+                        </div>
+                        <div className="btn-group">
+                          <span
+                            onClick={() =>
+                              handleAddRemoveAnswer("ADD", question.id)
+                            }
+                          >
+                            <FaPlus className="icon-add" />
+                          </span>
+                          {question.answers.length > 1 && (
+                            <span
+                              onClick={() =>
+                                handleAddRemoveAnswer(
+                                  "REMOVE",
+                                  question.id,
+                                  answer.id
+                                )
+                              }
+                            >
+                              <FaMinus className="icon-remove" />
+                            </span>
+                          )}
+                        </div>
+                      </div>
+                    );
+                  })}
+              </div>
+            );
+          })}
       </div>
     </div>
   );
